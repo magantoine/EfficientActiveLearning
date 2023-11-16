2466517ffd6ba3462ce4169b3ec4f4d172e11edf
Antoine Magron <antoine.magron@epfl.ch>
2023-11-16 13:43:14 +0100

    big changes

--- utils.py
+++ utils.py
@@ -14,20 +14,24 @@ def read_twitter_file(path):
     return tweets
 
 
-def create_datasets(sub_sampling=None):
+def create_datasets(sub_sampling=None, return_type="ds"):
+    assert(return_type in ["ds", "df"], "Return type either 'df' for DatFrame or 'ds' for Dataset")
     tnf = read_twitter_file(TWITTER_DIR + "train_neg_full.txt")
     tnf_labels = np.zeros(len(tnf))
     tpf = read_twitter_file(TWITTER_DIR + "train_pos_full.txt")
     tpf_labels = np.ones(len(tnf))
 
+    
     ds = Dataset.from_pandas(pd.concat([
         pd.DataFrame({'tweet': tnf, 'labels': tnf_labels}),
         pd.DataFrame({'tweet': tpf, 'labels': tpf_labels}),
-
-    ]).set_index("tweet")).shuffle()
+        ]
+    ).set_index("tweet")).shuffle()
 
     if(sub_sampling is not None):
-        ds = Dataset.from_dict(ds[:sub_sampling])
+        ds = ds[:sub_sampling]
+
+    cast_class = Dataset if(return_type == 'ds') else pd.DataFrame
     
-    return ds
+    return cast_class.from_dict(ds)
 
